1ee71c5612d8911c459f2ab7fde0ecfd02881776
0ky3r43m78m070b@gmail.com <0ky3r43m78m090b@gmail.com>
2020-04-26 16:16:02 +0900

    fix

--- s3.tf
+++ s3.tf
@@ -62,7 +62,7 @@ data "aws_iam_policy_document" "alb_log" {
     principals {
       type = "AWS"
       identifiers = [
-        "arn:aws:iam::${data.aws_elb_service_account.alb_log.id}:root",
+        "arn:aws:iam::${data.aws_elb_service_account.alb_log.id}:root", # 582318560864
       ]
     }
   }
